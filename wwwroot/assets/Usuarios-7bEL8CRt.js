5fa94ce54ee4e7f4b5b08557687db77e28b84616
Koko <Jose19lopez12@hotmail.com>
2025-08-06 11:03:56 -0600

    feat: actualizo build y fronten3d

--- wwwroot/assets/Usuarios-7bEL8CRt.js
+++ wwwroot/assets/Usuarios-7bEL8CRt.js
@@ -1,4 +1,4 @@
-import{_ as uo,r as _e,k as ti,o as fo,G as go,c as Wt,a as Nt,b as po,d as w,h as mo,v as bo,f as xo,t as it,e as _o,F as yo,g as vo,n as ye}from"./index-8ne8083D.js";import{a as Mo}from"./index-Dq7h7Pqt.js";import{N as ko}from"./NavbarAdmin-CxXWGnAI.js";/*!
+import{_ as uo,r as _e,k as ti,o as fo,G as go,c as Wt,a as Nt,b as po,d as w,h as mo,v as bo,f as xo,t as it,e as _o,F as yo,g as vo,n as ye}from"./index-C0pr5oqr.js";import{a as Mo}from"./index-Dq7h7Pqt.js";import{N as ko}from"./NavbarAdmin-CcWSepei.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
